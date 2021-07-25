f247f53c3e786066d4553904e698298ce7c00023
Marcelo <marcelocdias93@gmail.com>
2021-07-25 07:13:31 -0300

    Add style to articles index view

--- config/initializers/kaminari_config.rb
+++ config/initializers/kaminari_config.rb
@@ -3,10 +3,10 @@
 Kaminari.configure do |config|
   config.default_per_page = 20
   # config.max_per_page = nil
-  config.window = 0
+  config.window = 1
   config.outer_window = 0
-  config.left = 1
-  config.right = 3
+  config.left = 3
+  config.right = 1
   # config.page_method_name = :page
   # config.param_name = :page
   # config.max_pages = nil
