393a2f0785109778b443ebaaf3c2613a891574d8
Marcelo <marcelocdias93@gmail.com>
2021-07-25 23:40:19 -0300

    Correct tests

--- spec/factories/articles.rb
+++ spec/factories/articles.rb
@@ -2,11 +2,9 @@ require 'factory_bot'
 
 FactoryBot.define do
   factory :article do
-    source { Faker::Lorem.word }
+    source { Article.sources.keys.sample }
     title { Faker::Lorem.sentence }
     url { Faker::Internet.url }
-    published_at { Faker::Date.backward(days: 15)
-                              .to_s }
     collected_at { Faker::Date.backward(days: 14) }
     body { Faker::Lorem.paragraph }
   end
--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe Article, type: :model do
   it { should validate_presence_of(:source) }
   it { should validate_presence_of(:title) }
   it { should validate_presence_of(:url) }
-  it { should validate_presence_of(:published_at) }
   it { should validate_presence_of(:collected_at) }
 
   it 'has valid factory' do
